ce171baebd673409740ef909bd4c4209c18c8ae9
Pablo Hurtado <phurtado127@gmail.com>
2023-02-08 08:01:49 -0600

    sumaPablo Hurtado

---
+++ src/suma.java
@@ -0,0 +1,10 @@
+public class suma {
+
+
+    public static void main(String[] args) {
+        int a=2;
+        int b=2;
+        int suma=a+b;
+        System.out.println("suma resultado: "+suma);
+    }
+}
